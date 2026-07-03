15fa9de302c7002a3eba8466c272a4b9c93be5a7
ecosaprojects <ecosaprojects@gmail.com>
2026-07-03 21:46:05 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 
-import Header from './components/Header';
+import Header from './components/Header'
 
-import Home from './pages/Home';
-import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
-import MembersList from './pages/MembersList';
-import MemberProfile from './pages/MemberProfile';
-import Payments from './pages/Payments';
-import Projects from './pages/Projects';
-import JobBoard from './pages/JobBoard';
-import Community from './pages/Community';
-import Leaders from './pages/Leaders';
-import Resources from './pages/Resources';
+import Home from './pages/Home'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
+import MembersList from './pages/MembersList'
+import MemberProfile from './pages/MemberProfile'
+import Payments from './pages/Payments'
+import Projects from './pages/Projects'
+import JobBoard from './pages/JobBoard'
+import Community from './pages/Community'
+import Leaders from './pages/Leaders'
+import Resources from './pages/Resources'
+import Chapters from './pages/Chapters'
 
 export default function App() {
   return (
@@ -35,20 +36,23 @@ export default function App() {
           <Route path="/members" element={<MembersList />} />
           <Route path="/members/:id" element={<MemberProfile />} />
 
+          {/* Chapters */}
+          <Route path="/chapters" element={<Chapters />} />
+
+          {/* Community */}
+          <Route path="/community" element={<Community />} />
+
           {/* Payments */}
           <Route path="/payments" element={<Payments />} />
+
+          {/* Leaders */}
+          <Route path="/leaders" element={<Leaders />} />
 
           {/* Projects */}
           <Route path="/projects" element={<Projects />} />
 
           {/* Jobs */}
           <Route path="/jobs" element={<JobBoard />} />
-
-          {/* Community */}
-          <Route path="/community" element={<Community />} />
-
-          {/* Leaders */}
-          <Route path="/leaders" element={<Leaders />} />
 
           {/* Resources */}
           <Route path="/resources" element={<Resources />} />
@@ -66,5 +70,5 @@ export default function App() {
         </Routes>
       </div>
     </>
-  );
+  )
 }
